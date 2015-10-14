a30c1ea3626b98b6d3dc46e896eed8481b159706
BgGrimm <numizmatika@abv.bg>
2015-10-15 02:09:15 +0300

    initial commit

---
+++ Problem7 FirstAndLastName/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class FirstAndLastName
+{
+    public static void Main()
+    {
+        Console.WriteLine("Bob");
+        Console.WriteLine("Dylan");
+    }
+}
---
+++ Problem8 SquareRoot/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class SquareRoot
+{
+    public static void Main()
+    {
+        int number = 12345;
+        double sqrtNumber = Math.Sqrt(number);
+        Console.WriteLine(sqrtNumber);
+    }
+}
